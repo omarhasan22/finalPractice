b06a6c318e68f3020aec6019ed5bf3127bf804d9
your <omar.hasan3894@gmail.com>
2022-11-25 23:09:56 +0200

    First Commit

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ fetch(' https://finalpractice202103069.herokuapp.com/')
   .then(json => {
 
 
-    mainContainer = document.getElementById('mainContainer');
+    mainContainer = document.getElementById('editedContainer');
     console.log(json.services)
     
       
         
      var originalContent = ` 
-                            
+                                  
+                                  <div class="card">
+                                    <img src="assets/img/featured-1.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title"><a href="">Autem sunt earum</a></h5>
+                                      <p class="card-text">Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore</p>
+                                      <a href="#" class="btn">Explore more</a>
+                                    </div>
+                                  </div>
+                                
+
                            `;
  
  json.services.forEach(e => {
@@ -19,7 +29,7 @@ fetch(' https://finalpractice202103069.herokuapp.com/')
    currentContent = currentContent.replace(`[ICON]`,e["C"])
  
    var div = document.createElement('div')
-   div.className = "closed-item"
+   div.className = "col-lg-4 col-md-6 d-flex align-items-stretch mb-5 mb-lg-0"
    div.innerHTML = currentContent;
    mainContainer.appendChild(div);
  });
